e37a4bcbba13cd855afd6946f501a812ffa98ea5
Stark <serj.elessar@gmail.com>
2022-07-09 00:48:33 +0300

    create show and hode burger-menu

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const burgerBtn = document.querySelector('.burger-btn')  
+  const burgerBtn = document.querySelector('.burger-btn')
+  const closeBurgerMenu = document.querySelector('.burger-menu__close-btn')
+  const burgerMenu = document.querySelector('.burger-menu')
+  const popupWrapper = document.querySelector('.popup__wrapper')
+
+  if (burgerBtn || burgerMenu) {
+    burgerBtn.addEventListener('click', showBurgerMenu)
   
-  if (burgerBtn) {
-    burgerBtn.addEventListener ('click', () => {
-      burgerBtn.classList.toggle('active')
+    closeBurgerMenu.addEventListener('click', hideBurgerMenu)
+    popupWrapper.addEventListener('click', (e) => {
+      if (e.target && e.target.classList.contains('popup__wrapper')) {
+        hideBurgerMenu()
+      }
     })
+  }
+
+  function hideBurgerMenu() {
+    burgerBtn.classList.remove('active')
+    popupWrapper.classList.remove('show')
+    burgerMenu.classList.remove('show')
+  }
+  function showBurgerMenu() {
+    burgerBtn.classList.add('active')
+    popupWrapper.classList.add('show')
+    burgerMenu.classList.add('show')
   }
 })
